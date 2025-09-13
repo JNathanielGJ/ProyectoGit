0614bc9d47c436a3006b7d1500281c67fd4a9f51
Nathaniel Garcia <joejngj@gmail.com>
2025-09-13 12:18:41 -0600

    Cuarta versión

--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -3,9 +3,8 @@ package proyectogit;
 public class ProyectoGit {
 
     public static void main(String[] args) {
-       System.out.println("Hola Mundo");
-       System.out.println("Tercera");
-       
+        System.out.println("Ingresando Datos a github");
+        System.out.println("Recordar el orden: git add . - git commit -m - git push");
     }
     
 }
