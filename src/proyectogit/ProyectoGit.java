4809b6c1a7a7b56630887b29ab7f269842262750
Nathaniel Garcia <joejngj@gmail.com>
2025-09-13 12:15:35 -0600

    Segundo commit

--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectogit;
 
-/**
- *
- * @author TDFM
- */
 public class ProyectoGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hola Mundo");
+       System.out.println("Segunda versión");
     }
     
 }
